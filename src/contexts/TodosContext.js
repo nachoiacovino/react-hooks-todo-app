5909e242604b91863a74651c1620c46faa1815d5
Ignacio Iacovino <nacho.iacovino@gmail.com>
2019-11-30 00:18:52 +0100

    Fix TodosContext

--- src/contexts/TodosContext.js
+++ src/contexts/TodosContext.js
@@ -4,10 +4,8 @@ import useTodoState from '../hooks/useTodoState'
 const TodosContext = createContext()
 
 export const TodosProvider = props => {
-    const initialTodos = JSON.parse(window.localStorage.getItem("todos") || "[]") 
-
     // const { todos, addTodo, removeTodo, toggleTodo, updateTodo } = useTodoState(initialTodos)
-    const todosStuff = useTodoState(initialTodos)
+    const todosStuff = useTodoState([])
     
     return (
         <TodosContext.Provider value={todosStuff}>
